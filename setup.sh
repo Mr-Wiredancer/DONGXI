9c58e0b51e8c627445fa7daf20488791c1dbd765
Allen Wu <allenwu1018@gmail.com>
2013-10-22 16:39:16 +0800

    add setup script

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+DB_EXIST=`psql -l | grep dongxi_development | wc -l`
+RAILS_ROOT=`pwd`
+
+cp -a $RAILS_ROOT/config/application.example.yml $RAILS_ROOT/config/application.yml
+
+bundle install
+
+if [ $DB_EXIST == 1 ]; then
+  echo "Drop original database"
+  rake db:drop
+else
+  echo "Do nothing yeah."
+fi
+
+# setup db
+rake db:create && rake db:migrate && rake db:seed
+
+
