e70aa370b1b6885590d6f837b1680605c75936dd
Allen Wu <allenwu1018@gmail.com>
2013-10-26 14:43:02 +0800

    fix..

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
 # encoding: utf-8
 Project.delete_all
+ProjectBasicInfo.delete_all
+ProjectStory.delete_all
 Category.delete_all
 Sponsor.delete_all
 Region.delete_all
-
 
 %w(慈善 体育 艺术).each do |cat_name|
   Category.create(name: cat_name, description: "#{cat_name}的描述")
@@ -13,11 +14,10 @@ end
   Sponsor.create(name: sponsor_name, description: "#{sponsor_name}的描述")
 end
 
-profile = Project.create(name: "DongXi Tech",
-                         introduction: "DONGXI TECH 由来自Berkeley的Jiahao Li，圣三一学院的Water，和来自Sun Yat-sen Univ的Allen Wu倾情打造（说的很高端似的……）\n我们主要的想法就是做一个东西。\n 暂时是这个。",
-                         category_id: Category.first.id,
-                         sponsor_id: Sponsor.first.id
-                        )
+project = Project.new(category_id: Category.first.id, sponsor_id: Sponsor.first.id)
+project.basic_info = ProjectBasicInfo.create(name: "DongXi Tech")
+project.story = ProjectStory.create(introduction: "DONGXI TECH 由来自Berkeley的Jiahao Li，圣三一学院的Water，和来自Sun Yat-sen Univ的Allen Wu倾情打造（说的很高端似的……）\n我们主要的想法就是做一个东西。\n 暂时是这个。")
+project.save
 
 %w(广州 上海 北京).each do |name|
   Region.create(name: name, description: name)
