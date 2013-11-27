899daf6d235bb373010da09f16ba437fe31961a3
Allen Wu <allenwu1018@gmail.com>
2013-11-27 12:18:25 +0800

    Add photo and avatar in project/:id/show(preview) page
    https://basecamp.com/2438422/projects/4181244/todos/72301366-1h-12-00-cost
    
    The upload function is okay. But show / preview page didn't display the
    uploaded image. Fixed now.

--- app/helpers/project_helper.rb
+++ app/helpers/project_helper.rb
@@ -1,14 +1,28 @@
 # coding: utf-8
 module ProjectHelper
 
-  def render_project_introduction(project)
-    !!project.introduction ? content_tag(:div, raw(project.introduction), class: "description row") : nil
+  ### story & info
+  def render_story_and_info(project)
+    output = ""
+    output = output + render_project_photo(project) + render_project_video(project) + render_project_introduction(project)
+    output.html_safe
   end
-
   def render_project_video(project)
-    !!project.video_url ? content_tag(:div, raw(project.video_url), class: "video row") : nil
+    project.video_url ? content_tag(:div, raw(project.video_url), class: "video row") : ""
+  end
+  def render_project_introduction(project)
+    project.introduction ? content_tag(:div, raw(project.introduction), class: 'description row') : ""
+  end
+  def render_project_photo(project)
+    project.photo ? image_tag(project.photo.url(:normal)) : ""
   end
 
+  ### owner
+  def render_project_owner(project)
+    image_tag @project.owner_avatar.try(:url, :normal)
+  end
+
+  ### status
   def render_project_status(project)
     content_tag(:div, ("还差" + content_tag(:span, '38个分享', class: 'share-count')).html_safe, class: 'row') +
     content_tag(:div, "该项目就可进入公共筹集", class: 'row') +
@@ -16,15 +30,29 @@ module ProjectHelper
     render_progress(project)
   end
 
+  ### SNS
   def render_project_weibo(project)
-    if project.weibo_status
-      render_weibo_status(project.weibo_status.status_id) +
-      render_weibo_share(project)
-    else
-      nil
+    project.weibo_status ? render_weibo_status(project.weibo_status.status_id) + render_weibo_share(project) : ""
+  end
+  def render_weibo_status(sid)
+    content_tag :div, class: 'row' do
+      render :partial => "common/weibo_status", locals: { id: sid }
+    end
+  end
+  def render_weibo_share(project)
+    content_tag :div, class: 'row' do
+      render  :partial => "common/weibo_share_button",
+              :locals  => {
+                :url      => request.original_url,
+                :pic_url  => "http%3A%2F%2Fstatic.youku.com%2Findex%2Fimg%2Fheader%2Fyklogo.png",
+                :tag      => "东西科技", # slogan?
+                :text     => project.try(:basic_info).try(:name),
+                :mid      => project.weibo_status.status_mid,
+              }
     end
   end
 
+  ### donation
   def render_donate_box(project)
     content_tag :div, class: 'row' do
       link_to 'http://me.alipay.com/allenfantasy', target: '_blank' do
@@ -32,13 +60,13 @@ module ProjectHelper
       end
     end
   end
-
   def render_donate_form(project)
     render :partial => "donate_form", locals: { project: project }
   end
 
+  ### others
   def render_project_weibo_meta(project)
-    render :partial => "weibo_meta", locals: { project: @project }
+    render :partial => "weibo_meta", locals: { project: project }
   end
 
   def project_raise_type_options
@@ -77,22 +105,4 @@ module ProjectHelper
     content_tag :span, nil, class: "speed #{flag}", style:"width: #{percentage}%"
   end
 
-  def render_weibo_status(sid)
-    content_tag :div, class: 'row' do
-      render :partial => "common/weibo_status", locals: { id: sid }
-    end
-  end
-
-  def render_weibo_share(project)
-    content_tag :div, class: 'row' do
-      render  :partial => "common/weibo_share_button",
-              :locals  => {
-                :url      => request.original_url,
-                :pic_url  => "http%3A%2F%2Fstatic.youku.com%2Findex%2Fimg%2Fheader%2Fyklogo.png",
-                :tag      => "东西科技", # slogan?
-                :text     => project.try(:basic_info).try(:name),
-                :mid      => project.weibo_status.status_mid,
-              }
-    end
-  end
 end
