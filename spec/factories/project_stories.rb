1dc4a7c1edb89311294c8015ab304469025025cf
Allen Wu <allenwu1018@gmail.com>
2013-11-29 15:40:51 +0800

    try to skip after_save :save_weibo_id in rspec to prevent Faraday error.

--- spec/factories/project_stories.rb
+++ spec/factories/project_stories.rb
@@ -4,5 +4,7 @@ FactoryGirl.define do
     weibo_url     "http://weibo.com/3876426391/AiFnt8zMh"
     video_url     "<iframe height=596 width=620 src='http://player.youku.com/embed/XMzk1NjM4MzMy' frameborder=0 allowfullscreen></iframe>"
     risk          "risk"
+
+    after(:build) { |story| story.class.skip_callback(:save, :after, :save_weibo_id) }
   end
 end
